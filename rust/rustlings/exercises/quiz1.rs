08ea222586b615069aee4f60c5683d4a8c142669
Michael Hume <michael.a.hume@pm.me>
2023-01-15 22:42:14 -0500

    finish if stuff

--- rust/rustlings/exercises/quiz1.rs
+++ rust/rustlings/exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+pub fn calculate_price_of_apples(numberOfApples: i32) -> i32 {
+    let price = 2;
+    let discount_price = 1;
+    let discount = 40;
+
+    if numberOfApples > discount {
+        numberOfApples * discount_price
+    } else {
+        numberOfApples * price
+    }
+}
 
 // Don't modify this function!
 #[test]
